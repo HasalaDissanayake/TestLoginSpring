832978c2244e965bf3b2cb4d851aa87914ac1030
Hasala <hasaladissanayake@yahoo.com>
2023-07-01 19:42:34 +0530

    Initial commit

---
+++ src/main/java/com/example/logindemo/EmployeeController/EmployeeController.java
@@ -0,0 +1,26 @@
+package com.example.logindemo.EmployeeController;
+
+
+import com.example.logindemo.Dto.EmployeeDTO;
+import com.example.logindemo.Service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("api/v1/employee")
+
+
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @PostMapping(path = "/save")
+    public String saveEmployee(@RequestBody EmployeeDTO employeeDTO)
+    {
+        String id = employeeService.addEmployee(employeeDTO);
+        return id;
+    }
+
+}
